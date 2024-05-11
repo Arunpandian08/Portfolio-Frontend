8a49d925b61c558e4953af1cf15096e2e5ee5831
Arunpandian08 <arunpandianda123@example.com>
2024-05-11 15:43:55 +0530

    Frontend Project added

--- src/Utility/Data/data.js
+++ src/Utility/Data/data.js
@@ -3,7 +3,7 @@ export const data =[
         id: 1,
         image:'/Projects Images/DietApp-img.png',
         title:"Diet Suggestion App [CapStone Project]",
-        points:["Landing page","User Managements","Diet Recipes","Fat Burners","BMI & BMR Calculators","Diet Charts"],
+        points:["Landing page","User Managements","Diet Recipes","Fat Burners","BMI & BMR Calculators","Diet Charts[Chart.js Third-party library]"],
         frontend:"https://github.com/Arunpandian08/Diet-Suggestion-App-Frontend.git",
         backend:"https://github.com/Arunpandian08/Diet-suggestion-App-backend.git",
         demo:"https://diet-suggestion-app-frontend.netlify.app/"
@@ -16,5 +16,13 @@ export const data =[
         frontend:"https://github.com/Arunpandian08/Frontend-Todo-App-Context-API.git",
         backend:"https://github.com/Arunpandian08/Backend-Todo-App-Context-API.git",
         demo:"https://todo-app-with-context-api-08.netlify.app/"
+    },
+    {
+        id: 3,
+        image:'/Projects Images/Youtube_Clone.png',
+        title:"Youtube-Clone [Frontend]",
+        points:["Data From Youtube Data API","Youtube Home Page","Video Page","Recommended Videos","Using Moment.js Third-Party library","React Router DOM"],
+        frontend:"https://github.com/Arunpandian08/youtube-clone.git",
+        demo:"https://youtube-clone-frontend-f50664.netlify.app/"
     }
 ]
